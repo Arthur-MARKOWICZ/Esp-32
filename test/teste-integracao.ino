5d9376f90d6b80ec7867e36ba6c9238539b4d0bf
André Gabriel Benato <andregbenato@outlook.com>
2025-05-21 09:39:03 -0300

    Update teste-integracao.ino

--- test/teste-integracao.ino
+++ test/teste-integracao.ino
@@ -1,37 +1,51 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
 #include <DHT.h>
 
-#define DHTPIN 2
-#define DHTTYPE DHT22
-
-#define LED1 26
-#define LED2 35
+#define DHTPIN 4           
+#define DHTTYPE DHT22      
+#define WIFI_SSID "seu_SSID"
+#define WIFI_PASSWORD "sua_SENHA"
 
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
-  Serial.begin(115200);
-  dht.begin();
+AsyncWebServer server(80);
 
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
+void setup() {
+  // Inicializa o monitor serial
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando-se ao WiFi...");
+  }
+  Serial.println("Conectado ao WiFi");
+  Serial.print("IP do ESP32: ");
+  Serial.println(WiFi.localIP());
+
+  dht.begin();
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    float h = dht.readHumidity();  // Lê a umidade
+    float t = dht.readTemperature();  // Lê a temperatura em °C
+
+    if (isnan(h) || isnan(t)) {
+      request->send(500, "text/plain", "Falha ao ler o sensor DHT!");
+      return;
+    }
+
+    String htmlContent = "<html><body>";
+    htmlContent += "<h1>Temperatura e Umidade</h1>";
+    htmlContent += "<p>Temperatura: " + String(t) + " &deg;C</p>";
+    htmlContent += "<p>Umidade: " + String(h) + " %</p>";
+    htmlContent += "<p><a href='/'>Atualizar</a></p>";
+    htmlContent += "</body></html>";
+    request->send(200, "text/html", htmlContent);
+  });
+
+  // Inicia o servidor web
+  server.begin();
 }
 
 void loop() {
-  float temp = dht.readTemperature();
-  if (isnan(temp)) {
-    Serial.println("Falha na leitura do sensor DHT22");
-    return;
-  }
-  Serial.print("Temperatura: ");
-  Serial.print(temp);
-  Serial.println(" °C");
-  if (temp >= 20 && temp <= 25) {
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, LOW);
-  } else {
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, HIGH);
-  }
-
-  delay(2000);
 }
