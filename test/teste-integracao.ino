b375ce8614d18b8c66175230902ba6e4e8a87f1c
Arthur Markowicz Lopes <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-06-10 10:30:41 -0300

    Update teste-integracao.ino

--- test/teste-integracao.ino
+++ test/teste-integracao.ino
@@ -1,26 +1,26 @@
 #include <WiFi.h>
 #include <DHT.h>
-
-#define DHTPIN 4          // Pino onde o DHT22 está conectado
-#define DHTTYPE DHT22     // Tipo do sensor DHT22
-#define LED1 19            // LED 1
-#define LED2 23          // LED 2
+#include <vector>
+#define DHTPIN 4         
+#define DHTTYPE DHT22     
+#define LED1 19            
+#define LED2 23          
 
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
 
-DHT dht(DHTPIN, DHTTYPE);  // Cria o objeto DHT para o sensor
+DHT dht(DHTPIN, DHTTYPE);  
 
-WiFiServer server(80);  // Cria o servidor web na porta 80
-
+WiFiServer server(80); 
+std::vector<float> temperaturas;
 void setup() {
-  Serial.begin(115200);   // Inicializa o monitor serial
-  dht.begin();            // Inicializa o sensor DHT22
 
-  pinMode(LED1, OUTPUT);  // Configura LED1 como saída
-  pinMode(LED2, OUTPUT);  // Configura LED2 como saída
+  Serial.begin(115200);   
+  dht.begin();           
 
-  // Conecta à rede Wi-Fi
+  pinMode(LED1, OUTPUT);  
+  pinMode(LED2, OUTPUT);  
+
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -30,47 +30,56 @@ void setup() {
   Serial.print("IP do ESP32: ");
   Serial.println(WiFi.localIP());
 
-  // Inicia o servidor
   server.begin();
 }
 
 void loop() {
-  WiFiClient client = server.available();  // Verifica se um cliente fez uma requisição
+  WiFiClient client = server.available(); 
 
   if (client) {
     Serial.println("Novo cliente conectado");
-    String currentLine = "";  // Armazena a linha recebida do cliente
+    String currentLine = "";  
 
     while (client.connected()) {
       if (client.available()) {
-        char c = client.read();  // Lê o caractere recebido do cliente
+        char c = client.read();  
         Serial.write(c);
 
-        // Se encontrou uma linha em branco, significa que a requisição foi recebida
         if (c == '\n' && currentLine.length() == 0) {
-          // Lê a temperatura
+
           float temp = dht.readTemperature();
           float umidade = dht.readHumidity();
 
-          // Verifica se houve falha na leitura
-          if (isnan(temp)) {
+
+          if (isnan(temp) || isnan(umidade)) {
+
             client.println("HTTP/1.1 500 Internal Server Error");
             client.println("Content-Type: text/plain");
             client.println();
             client.println("Falha na leitura do sensor DHT22");
             break;
           }
-
-          // Controla os LEDs com base na temperatura
-          if (temp >= 20 && temp <= 25) {
-            digitalWrite(LED1, HIGH);  // Acende o LED1
-            digitalWrite(LED2, LOW);   // Apaga o LED2
+          if (temperaturas.size() >= 50) {
+            temperaturas.erase(temperaturas.begin());
+          }
+          temperaturas.push_back(temp);
+          float media_temp = 0;
+          if (!temperaturas.empty()) {
+            for (float t : temperaturas) {
+              media_temp += t;
+            }
+            media_temp /= temperaturas.size();
+          }
+        
+          if (temp >= 20 && temp <= 30) {
+            digitalWrite(LED1, HIGH);
+            digitalWrite(LED2, LOW);   
           } else {
-            digitalWrite(LED1, LOW);   // Apaga o LED1
-            digitalWrite(LED2, HIGH);  // Acende o LED2
+            digitalWrite(LED1, LOW);   
+            digitalWrite(LED2, HIGH);  
           }
 
-          // Responde com um conteúdo HTML
+    
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");
@@ -80,8 +89,13 @@ void loop() {
           client.println("<h1>Temperatura e Controle de LEDs</h1>");
           client.println("<p>Temperatura: " + String(temp) + " C</p>");
           client.println("<p>Umidade: " + String(umidade) + " % </p>");
+          if(temp>= 20 && temp <=30 && umidade >= 60){
+          client.println("<p>Condicoes estao favoraveis para formar mofo</p>");
+          } else{
+            client.println("<p>Condicoes desfavoraveis para formar mofo</p>");
+          }
 
-          if (temp >= 20 && temp <= 25) {
+          if (temp >= 20 && temp <= 30) {
             client.println("<p>LED 1 esta ACESO</p>");
             client.println("<p>LED 2 esta APAGADO</p>");
           } else {
@@ -99,7 +113,7 @@ void loop() {
         }
       }
     }
-    client.stop();  // Fecha a conexão com o cliente
+    client.stop(); 
     Serial.println("Cliente desconectado");
   }
 }
